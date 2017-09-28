6b83b6052f3b848c192424130c40c5e85b8808de
[Rick] <[469225039@qq,com]>
2017-09-28 17:57:31 +0800

    1

--- WebAutoTest/TestCase/test_order.py
+++ WebAutoTest/TestCase/test_order.py
@@ -225,16 +225,16 @@ if __name__ == '__main__':
     suit = unittest.TestSuite()
     case_list = [
                   TestCase('test_order_1'),
-                  TestCase('test_order_2'),
-                  TestCase('test_order_3'),
-                  TestCase('test_order_4'),
-                  TestCase('test_order_5'),
-                  TestCase('test_order_6'),
-                  TestCase('test_order_7'),
-                  TestCase('test_order_8'),
-                  TestCase('test_order_9'),
-                  TestCase('test_order_10'),
-                  TestCase('test_order_11'),
+                  # TestCase('test_order_2'),
+                  # TestCase('test_order_3'),
+                  # TestCase('test_order_4'),
+                  # TestCase('test_order_5'),
+                  # TestCase('test_order_6'),
+                  # TestCase('test_order_7'),
+                  # TestCase('test_order_8'),
+                  # TestCase('test_order_9'),
+                  # TestCase('test_order_10'),
+                  # TestCase('test_order_11'),
                   ]
     suit.addTests(case_list)
     # now = time.strftime("%Y_%m_%d %H_%M_%S")

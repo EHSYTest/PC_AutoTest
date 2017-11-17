c24d8f75297517154d68642bd7956ffb93df362e
vivien_tang <18551614507@163.com>
2017-11-17 17:02:46 +0800

    New

--- WebAutoTest/Page/Page_Home.py
+++ WebAutoTest/Page/Page_Home.py
@@ -55,7 +55,7 @@ class Home(Page):
         # self.switch_to_new_window()
 
     def brand_click(self):
-        self.element_find(self.brand_center).click()
+        self.wait_to_clickable(self.brand_center).click()
         self.element_find(self.brand_bosch).click()
 
     def go_user_center(self):

9b65fc33ff43075800a8982cff3f45c7396879d5
[Rick] <[469225039@qq,com]>
2017-11-20 15:15:02 +0800

    New

--- WebAutoTest/TestCase/run_test.py
+++ WebAutoTest/TestCase/run_test.py
@@ -6,7 +6,6 @@ discover = unittest.defaultTestLoader.discover(test_dir, pattern='test*.py')
 
 
 if __name__ == '__main__':
-    # now = time.strftime("%Y_%m_%d %H_%M_%S")
     file = open('../TestResult/order.html', 'wb')
     runner = HTMLTestRunner(stream=file, title='WWW测试报告', description='测试情况')
     runner.run(discover)

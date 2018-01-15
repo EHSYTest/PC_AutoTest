b3ef7165082eecb237793e3e3e335594bb869fd8
vivien_tang <18551614507@163.com>
2018-01-15 17:16:17 +0800

    更新

--- WebAutoTest/TestCase/test_price.py
+++ WebAutoTest/TestCase/test_price.py
@@ -172,10 +172,10 @@ class TestPrice(unittest.TestCase):
         self.driver.quit()
 
 if __name__ == '__main__':
-    # unittest.main()
-    suite = unittest.TestSuite()
-    suite.addTest(TestPrice('test_price_12'))
-    file = open('../TestResult/order.html', 'wb')
-    runner = HTMLTestRunner(stream=file, title='WWW下单——测试报告', description='测试情况')
-    runner.run(suite)
-    file.close()
+    unittest.main()
+    # suite = unittest.TestSuite()
+    # suite.addTest(TestPrice('test_price_12'))
+    # file = open('../TestResult/order.html', 'wb')
+    # runner = HTMLTestRunner(stream=file, title='WWW下单——测试报告', description='测试情况')
+    # runner.run(suite)
+    # file.close()

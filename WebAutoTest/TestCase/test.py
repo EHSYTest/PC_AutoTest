3ae9716fcf5956caa4fe1ff7600894a5bac0164e
[Rick] <[469225039@qq,com]>
2018-02-23 10:00:32 +0800

    By Rick

--- WebAutoTest/TestCase/test.py
+++ WebAutoTest/TestCase/test.py
@@ -1,16 +1,31 @@
 import pytest, allure
 
+def pytest_configure(config):
+    allure.environment(report='Allure report', browser='Chrome 63')
+
 class Test_a():
     # @pytest.fixture('module', autouse=True)
     def setup_method(self, method):
         self.a = 'a'
         print('setup')
 
+    @allure.feature('Feature1')
+    @allure.story('Story1')
     def test_a(self):
-        print(self.a)
+        with pytest.allure.step('打印a'):
+            print(self.a)
+        with pytest.allure.step('结束'):
+            pass
 
+    @allure.feature('Feature1')
+    @allure.story('Story2')
     def test_b(self):
         print(self.a+'------------')
+
+    @allure.feature('Feature2')
+    @allure.story('Story1')
+    def test_c(self):
+        print(self.a + '------------')
 
     @staticmethod
     def teardown_method(method):

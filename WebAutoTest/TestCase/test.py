37e3c91ab0abdb9fab4ccea2e3b4147a44533837
Rick_Zhang <rick_zhang@ehsy.com>
2018-03-09 16:00:11 +0800

    By Rick

--- WebAutoTest/TestCase/test.py
+++ WebAutoTest/TestCase/test.py
@@ -1,21 +1,19 @@
 from selenium import webdriver
 import unittest
 from Page_Base import Page
+from xmlrpc import client
 
 
-class TestCase(unittest.TestCase):
-    def setUp(self):
-        self.driver = webdriver.Chrome()
-        self.driver.get('http://ps.ehsy.com')
-        self.driver.implicitly_wait(10)
-        self.driver.maximize_window()
+dbname = 'odoo-staging'
+usr = 'admin'
+pwd = 'admin'
+# oe_ip = 'odoo-staging.ehsy.com'
+oe_ip = 'localhost:8069'
 
-    def test_a(self):
-        self.driver.find_element_by_xpath('//*[@id="js-logininfo"]/div[1]/div/a[1]')
+vals = {'ks_no': 'KS2018030763'}
 
-    def tearDown(self):
-        self.driver.quit()
-
-
-if __name__ == '__main__':
-    unittest.main()
+sock_common = client.ServerProxy('http://' + oe_ip + '/xmlrpc/common')
+uid = sock_common.login(dbname, usr, pwd)
+sock = client.ServerProxy('http://' + oe_ip + '/xmlrpc/object')
+result = sock.execute(dbname, uid, pwd, 'used.by.tester', 'so_invoice', vals)
+print(result)

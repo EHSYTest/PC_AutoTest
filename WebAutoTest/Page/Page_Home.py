02ec811184904a5421fcf201d675ee469788d1b1
Rick_Zhang <rick_zhang@ehsy.com>
2018-04-08 15:40:03 +0800

    By Rick

---
+++ WebAutoTest/Page/Page_Home.py
@@ -0,0 +1,83 @@
+from Page_Base import Page
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import ElementNotVisibleException, WebDriverException
+from selenium.webdriver.common.by import By
+import allure, pytest
+
+class Home(Page):
+    """首页"""
+    login_button = (By.XPATH, '//*[@id="js-logininfo"]/div[1]/div/a[1]')
+    logout_button = (By.XPATH, '//div[1]/span/a[2]')
+    username_send = (By.NAME, 'username')
+    password_send = (By.NAME, 'password')
+    login_action = (By.CLASS_NAME, 'loginpop-btn')
+    my_ehsy = (By.CLASS_NAME, 'ehsy-a')
+
+    # 个人中心
+    my_address = (By.CLASS_NAME, 'active')  # 进入我的地址标签页
+
+    category_tool = (By.XPATH, '//ul/li[1]/a[1]/span')
+    category_taozhuang = (By.LINK_TEXT, '综合套装')
+    category_knife = (By.XPATH, '//li[2]/a[1]/span')
+
+    powerchina_l1_category = (By.LINK_TEXT, '主打装备 成套产品')
+    powerchina_l2_category = (By.LINK_TEXT, '护理床')
+
+    # my_ehsy = (By.CLASS_NAME, 'my-ehsy-show')
+    # my_collection = (By.CLASS_NAME, 'header-my-collection')
+    quick_order = (By.CLASS_NAME, 'member-discount')
+
+    search_send = (By.NAME, 'search')
+    search_button = (By.CLASS_NAME, 'btn-search')
+
+    my_cart = (By.CLASS_NAME, 'cart-wrap')
+
+    brand_center = (By.LINK_TEXT, '品牌中心')
+    brand_bosch = (By.XPATH, "//div[5]/ul[1]/li[1]/a/img")
+
+    layer = (By.ID, 'ajax-layer-loading')
+
+    cxml_url = (By.XPATH, '//url')
+
+    def login(self, login_name, password):
+        with allure.step('登录'):
+            self.wait_click(self.login_button)
+            self.element_find(self.username_send).send_keys(login_name)
+            self.element_find(self.password_send).send_keys(password)
+            self.wait_click(self.login_action)
+
+    def login_other(self, login_name, password):
+        with allure.step('登录'):
+            self.element_find(self.username_send).send_keys(login_name)
+            self.element_find(self.password_send).send_keys(password)
+            self.element_find(self.login_action).click()
+
+    def category_tree_click(self, l1_category=category_tool, l2_category=category_taozhuang):
+        with allure.step('点击产线进入产品列表页'):
+            l1_category = self.element_find(l1_category)
+            ActionChains(self.driver).move_to_element(l1_category).perform()
+            self.wait_click(l2_category)
+
+    def search_sku(self, sku='MAD618'):
+        with allure.step('搜索SKU'):
+            allure.attach('SKU', sku)
+            self.element_find(self.search_send).send_keys(sku)
+            self.wait_click(self.search_button)
+
+    def quick_order_click(self):
+        with allure.step('进入快速下单页'):
+            self.wait_click(self.quick_order)
+
+    def brand_click(self):
+        with allure.step('进入品牌页'):
+            self.wait_click(self.brand_center)
+            self.wait_click(self.brand_bosch)
+
+    def go_user_center(self):
+        with allure.step('进入个人中心'):
+            self.wait_click(self.my_ehsy)
+
+
+
+
+
---
+++ WebAutoTest/TestCase/test_cart.py
@@ -0,0 +1,99 @@
+import unittest, sys
+sys.path.append('../Page')
+from selenium import webdriver
+from HTMLTestRunner import HTMLTestRunner
+from Page_Base import Page
+from Page_NormalCart import NormalCart
+from Page_Home import Home
+from Page_ProductList import ProductList
+import pytest, allure
+
+
+@allure.feature('购物车页面测试')
+@pytest.allure.severity(pytest.allure.severity_level.MINOR)
+class TestCart(unittest.TestCase):
+
+    def setup_method(self, method):
+        with allure.step('---Start---'):
+            self.driver = webdriver.Chrome()
+            self.page = Page(self.driver)
+            self.environment = self.page.config_reader('environment.conf', 'Environment', 'environment')
+            if self.environment == 'staging':
+                self.url = 'http://ps.ehsy.com'
+                self.driver.get(self.url)
+            else:
+                self.url = 'http://www.ehsy.com'
+                self.driver.get(self.url)
+            self.driver.implicitly_wait(30)
+            self.driver.maximize_window()
+            self.home = Home(self.driver)
+            self.productList = ProductList(self.driver)
+            self.normal_cart = NormalCart(self.driver)
+            allure.attach('初始化参数:', 'environment: '+self.environment+'\nurl: '+self.url+'\n')
+
+    @allure.story('数量、复选框、商品详情跳转')
+    def test_cart_check(self):
+        # allure.environment(Report='Cart Report', Browser='Chrome 63', URL=self.url)
+        with allure.step('读取配置的普通产品SKU'):
+            sku = self.page.config_reader('data.conf', '普通商品', 'product')
+            allure.attach('SKU', sku)
+        self.home.search_sku(sku)
+        self.productList.searchResult_add_to_cart()
+        self.normal_cart.quantity_add_or_sub()
+        self.normal_cart.wait_to_stale(self.normal_cart.layer)
+        self.normal_cart.quantity_edit_check()
+        self.normal_cart.wait_to_stale(self.normal_cart.layer)
+        self.normal_cart.cart_checkboxs_select()
+        self.normal_cart.product_click()
+
+    # def test_cart_areaLimit(self):
+    #     ###购物车区域限制商品###
+    #     sku = self.page.config_reader('data.conf', '区域限制产品', 'product')
+    #     self.home.search_sku(sku)
+    #     self.productList.searchResult_add_to_cart()
+    #     self.normal_cart.area_limit_sku()
+
+    @allure.story('跳转报价单')
+    def test_cart_bj(self):
+        with allure.step('读取普通商品配置信息'):
+            sku = self.page.config_reader('data.conf', '普通商品', 'product')
+            allure.attach('SKU: ', sku)
+        self.home.search_sku(sku)
+        self.productList.searchResult_add_to_cart()
+        self.normal_cart.bj_page()
+
+    @allure.story('未登录—>登录购物车SKU合并')
+    def test_cart_combine(self):
+        with allure.step('读取普通商品配置信息'):
+            sku = self.page.config_reader('data.conf', '普通商品', 'product')
+            allure.attach('SKU: ', sku)
+        self.home.search_sku(sku)
+        self.productList.searchResult_add_to_cart()
+        self.normal_cart.cart_combine()
+
+    @allure.story('商品删除')
+    def test_cart_delete(self):
+        with allure.step('读取普通商品配置信息'):
+            sku = self.page.config_reader('data.conf', '区域限制产品', 'product')
+            allure.attach('SKU: ', sku)
+        self.home.search_sku(sku)
+        self.productList.searchResult_add_to_cart()
+        with allure.step('读取账号配置信息'):
+            loginname = self.page.config_reader('test_order.conf', '个人账号', 'login_name')
+            password = self.page.config_reader('test_order.conf', '个人账号', 'password')
+            allure.attach('账号: ', 'loginname: '+loginname+'\npassword: '+password)
+        with allure.step('登录'):
+            self.home.login(loginname, password)
+        self.normal_cart.cart_delete()
+
+    def teardown_method(self, method):
+        test_method_name = self._testMethodName
+        with allure.step('保存截图'):
+            self.driver.save_screenshot('../TestResult/ScreenShot/%s.png' % test_method_name)
+            f = open('../TestResult/ScreenShot/%s.png' % test_method_name, 'rb').read()
+            allure.attach('自动化截图', f, allure.attach_type.PNG)
+        with allure.step('End'):
+            self.driver.quit()
+
+
+

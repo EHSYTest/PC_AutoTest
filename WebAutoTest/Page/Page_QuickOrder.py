1fe1689865b0e41332c7b54cfc09537fef24712e
Rick_Zhang <rick_zhang@ehsy.com>
2018-04-25 14:13:17 +0800

    By Rick

--- WebAutoTest/Page/Page_QuickOrder.py
+++ WebAutoTest/Page/Page_QuickOrder.py
@@ -4,6 +4,7 @@ from selenium.common.exceptions import StaleElementReferenceException, ElementNo
 from selenium.webdriver.common.by import By
 import allure, pytest
 
+
 class QuickOrder(Page):
     """快速下单页"""
     sku_send = (By.XPATH, '//tr[1]/td[2]/span/input')
@@ -12,8 +13,10 @@ class QuickOrder(Page):
     cart = (By.XPATH, '/html/body/div[1]/div[2]/div[3]/a')
     go_cart = (By.LINK_TEXT, '去购物车结算')
 
-    def quick_add_to_cart(self, product='MAA904'):
+    def quick_add_to_cart(self, product=''):
         with allure.step('快速下单页加入购物车'):
+            if not product:
+                product = self.config_reader('data.conf', '普通商品', 'product')
             self.element_find(self.sku_send).send_keys(product)
             self.element_find(self.quantity_send).send_keys(10)
             self.wait_click(self.add_to_cart)

5535a7deb428bdd871a0caac3ad90f39a31c4332
Rick_Zhang <rick_zhang@ehsy.com>
2018-06-12 20:24:02 +0800

    Merge branch 'develop2' into staging
    # Conflicts:
    #	.idea/workspace.xml
    #	WebAutoTest/TestCase/test_price.py

--- WebAutoTest/TestCase/test.py
+++ WebAutoTest/TestCase/test.py
@@ -1,20 +1,13 @@
-import pytest, random, sys
-sys.path.append('../Page')
-from Page_Base import Page
+a = 1
+b = 2
+x = 100000000000
+y = 100000000000
 
-
-sql = "SELECT SKU_CODE, promotion_value FROM pc.promotion_sku WHERE promotion_id IN(SELECT id FROM pc.promotion WHERE TYPE='1' AND promotion_type='1' AND `status`='1' and CURRENT_DATE BETWEEN start_date AND end_date ) AND CURRENT_DATE BETWEEN start_date AND end_date"
-page = Page('driver')
-cr = page.db_con('staging')
-cr.execute(sql)
-r = cr.fetchall()
-result = random.choice(r)
-print(r, result)
-sku = result['SKU_CODE']
-promotion_price = str(result['promotion_value'])
-sql = "SELECT market_price FROM pc.price WHERE CURRENT_DATE BETWEEN valid_time_start AND valid_time_end AND sku_code='"+sku+"'"
-cr.execute(sql)
-r = cr.fetchall()
+p = a*(10**(x-1))%b
+l = []
+for i in range(x, y+1):
+    zheng = str((p*10)//b)
+    p = (p*10)%b
+    l.append(zheng)
+r = ''.join(l)
 print(r)
-market_price = str(r[0]['market_price'])
-print({'sku': sku, 'promotion_price': promotion_price, 'market_price': market_price})

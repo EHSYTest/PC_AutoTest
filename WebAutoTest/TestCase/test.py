c6bc29fc3c26262e5184ea46eb7b2ab0d1658490
Rick_Zhang <rick_zhang@ehsy.com>
2018-06-20 10:30:18 +0800

    Merge branch 'develop2' into staging
    # Conflicts:
    #	.idea/workspace.xml
    #	WebAutoTest/TestCase/test_price.py

--- WebAutoTest/TestCase/test.py
+++ WebAutoTest/TestCase/test.py
@@ -1,15 +1,36 @@
-import unittest, sys
-sys.path.append('../Page')
-from selenium import webdriver
-from HTMLTestRunner import HTMLTestRunner
-from Page_Base import Page
-from Page_NormalCart import NormalCart
-from Page_Home import Home
-from Page_ProductList import ProductList
-import pytest, allure
+w = [1, 2]
+n = [2, 1]
+list_w = []
+for i in range(len(w)):
+    list_w_w = []
+    for j in range(n[i]+1):
+        list_w_w.append(w[i]*j)
+    list_w.append(list_w_w)
+r = []
+for x in range(len(list_w)-1):
+    for A in list_w[0]:
+        for B in list_w[1]:
+            r.append(A+B)
+    del list_w[0]
+    list_w[0] = r
+print(len(set(list_w[0])), set(list_w[0]))
 
-driver = 1
-sql = "select a.ORDER_ID from oc.order_info a where a.EXTERNAL_ORDER_NO='PR2018061220261792100'"
-sql_result = Page(driver).db_con('production', sql)
-SO = sql_result[0]['ORDER_ID']
-print(SO)
+# from functools import reduce
+#
+# def sum_list(a, b):
+#     return [m+n for m in a for n in b]
+# r = reduce(sum_list, list_w)
+# print(len(set(r)))
+
+
+
+
+
+# P = [[w[i]*k for k in range(n[i]+1)] for i in range(len(w))]
+#
+#
+# def set_sum(a,b):
+#     return [m+n for m in a for n in b]
+# L = reduce(set_sum,P)
+# # print len(set(L))
+# print(P, L)
